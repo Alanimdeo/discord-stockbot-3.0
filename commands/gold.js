1d5d9a90a833055766ec06a591812e594e5eeae4
Alanimdeo <alan@imdeo.kr>
2021-12-05 17:40:22 +0900

    - 금 구매 기능 추가 - 금 확인 기능 수정 - 금 가격 기능에 업데이트 시간 추가

--- commands/gold.js
+++ commands/gold.js
@@ -64,7 +64,8 @@ async function 가격(interaction, buyPrice, sellPrice) {
                               Math.round((Number(sellPrice.t) / sellPrice.y) * 10000) / 100 +
                               "%"
                     }]`
-                ),
+                )
+                .setFooter(`최근 갱신: ${buyPrice.x}`),
         ],
     });
 }
@@ -72,7 +73,8 @@ async function 가격(interaction, buyPrice, sellPrice) {
 async function 확인(interaction, buyPrice, sellPrice) {
     database.query(`SELECT gold FROM users WHERE id = ${interaction.member.id}`, async (err, result) => {
         if (err) console.error(err);
-        result = result[0];
+        result = JSON.parse(result[0].gold);
+        console.log(result);
         if (result.amount == 0)
             return await interaction.editReply({
                 embeds: [
@@ -82,18 +84,84 @@ async function 확인(interaction, buyPrice, sellPrice) {
                         .setDescription("보유한 금이 없습니다."),
                 ],
             });
-        else
+        else {
+            buyPrice = buyPrice.y;
+            var price = buyPrice * result.amount;
+            var difference = [
+                price - result.buyPrice,
+                ((price - result.buyPrice) / result.buyPrice / result.amount) * 100,
+            ];
             return await interaction.editReply({
                 embeds: [
                     new MessageEmbed()
                         .setColor("#ffff00")
                         .setTitle(`:coin: ${interaction.member.displayName} 님의 금 보유 현황`)
                         .setDescription(
-                            `수량: \`${result.amount.toLocaleString("ko-KR")}개\`\n(평균 구매가 ${(
+                            `수량: \`${result.amount.toLocaleString("ko-KR")}개\`\n평균 구매가: \`${(
                                 result.buyPrice / result.amount
-                            ).toLocaleString("ko-KR")}원)[]`
-                        ),
+                            ).toLocaleString("ko-KR")}원\`\n수익: \`${
+                                (difference[0] > 0 ? "+" : "") + difference[0].toLocaleString("ko-KR")
+                            }원 (${
+                                (difference[0] > 0 ? "+" : "") +
+                                (Math.floor(difference[1] * 100) / 100).toLocaleString("ko-KR")
+                            }%)\``
+                        )
+                        .setFooter(`최근 갱신: ${sellPrice.x}\n수익은 구매가 기준입니다.`),
                 ],
             });
+        }
+    });
+}
+
+async function 구매(interaction, buyPrice, sellPrice) {
+    await interaction.deferReply();
+    var amount = interaction.options.getInteger("수량");
+    if (amount < 1)
+        return await interaction.editReply({
+            embeds: [
+                new MessageEmbed()
+                    .setColor("#ff0000")
+                    .setTitle(":warning: 오류")
+                    .setDescription("1개 미만은 구매할 수 없습니다."),
+            ],
+        });
+    database.query(`SELECT money, gold FROM users WHERE id = ${interaction.member.id}`, async (err, result) => {
+        if (err) return console.error(err);
+        var money = result[0].money;
+        var gold = JSON.parse(result[0].gold);
+        buyPrice = buyPrice.y;
+        if (buyPrice * amount > money)
+            return await interaction.editReply({
+                embeds: [
+                    new MessageEmbed()
+                        .setColor("#ff0000")
+                        .setTitle(":warning: 오류")
+                        .setDescription("돈이 부족합니다."),
+                ],
+            });
+        money -= buyPrice * amount;
+        gold.amount += amount;
+        gold.buyPrice += buyPrice * amount;
+        database.query(
+            `UPDATE users SET money = ${money}, gold = ${JSON.stringify(gold)} WHERE id = ${interaction.member.id}`,
+            async (err) => {
+                await interaction.editReply({
+                    embeds: [
+                        new MessageEmbed()
+                            .setColor("#008000")
+                            .setTitle(":white_check_mark: 구매 완료")
+                            .setDescription(
+                                `금을 구매했습니다.\n구매 금액: \`${buyPrice.toLocaleString(
+                                    "ko-KR"
+                                )} × ${amount.toLocaleString("ko-KR")} = ${(buyPrice * amount).toLocaleString(
+                                    "ko-KR"
+                                )}원\`\n보유 중인 금: \`${gold.amount.toLocaleString(
+                                    "ko-KR"
+                                )}개\`\n남은 돈: \`${money.toLocaleString("ko-KR")}원\``
+                            ),
+                    ],
+                });
+            }
+        );
     });
 }
